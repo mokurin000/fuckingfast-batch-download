48ecb8324c0e3fdbd143cee910077f18d778867d
poly000 <1348292515a@gmail.com>
2024-10-01 15:20:24 +0800

    build: reuse environment for scrap-gui

--- build.ps1
+++ build.ps1
@@ -17,6 +17,10 @@ playwright install chromium
 pyinstaller @HIDDEN_DEPS --optimize 2 -D -n scrap-gui @CLEAN_OPT $HIDE_WINDOW src/$project/source/fitgirl/gui.py
 pyinstaller @HIDDEN_DEPS --optimize 2 -D -n extract-gui @CLEAN_OPT src/$project/gui.py
 
+# reuse library
+Move-Item .\dist\scrap-gui\scrap-gui.exe dist\extract-gui
+Remove-Item -Recurse -Force dist\scrap-gui
+
 # Optional, comment this to debug pyinstaller spec.
 Remove-Item *.spec
 # Clean build cache directory
