e163267911c51e4e771208f2a438c46883fc0f2a
poly000 <1348292515a@gmail.com>
2024-10-03 04:28:18 +0800

    build: package extract-gui-chromium without conflict

--- build-chromium.ps1
+++ build-chromium.ps1
@@ -14,12 +14,12 @@ $HIDDEN_DEPS = '--hidden-import=tkinter', '--collect-data=cli2gui'
 
 playwright install chromium
 
-pyinstaller @HIDDEN_DEPS --optimize 2 -D -n scrap-gui @CLEAN_OPT $HIDE_WINDOW src/$project/source/fitgirl/gui.py
-pyinstaller @HIDDEN_DEPS --optimize 2 -D -n extract-gui @CLEAN_OPT src/$project/gui.py
+pyinstaller @HIDDEN_DEPS --optimize 2 -D -n scrap-gui-chromium @CLEAN_OPT $HIDE_WINDOW src/$project/source/fitgirl/gui.py
+pyinstaller @HIDDEN_DEPS --optimize 2 -D -n extract-gui-chromium @CLEAN_OPT src/$project/gui.py
 
 # reuse library
-Move-Item .\dist\scrap-gui\scrap-gui.exe dist\extract-gui
-Remove-Item -Recurse -Force dist\scrap-gui
+Move-Item .\dist\scrap-gui-chromium\scrap-gui-chromium.exe dist\extract-gui-chromium
+Remove-Item -Recurse -Force dist\scrap-gui-chromium
 
 # Optional, comment this to debug pyinstaller spec.
 Remove-Item *.spec
