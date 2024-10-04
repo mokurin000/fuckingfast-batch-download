a19bf2cd333f22a42e8b096bbad2069decb47923
poly000 <1348292515a@gmail.com>
2024-10-04 21:54:33 +0800

    fix: don't be unresponsive after Start

--- src/fuckingfast_batch_download/gui.py
+++ src/fuckingfast_batch_download/gui.py
@@ -1,11 +1,27 @@
+from threading import Thread
+
+import xdialog
 from cli2gui import Cli2Gui
 
 from fuckingfast_batch_download.__main__ import blocking_run, main
 
+THREAD: Thread = None
+
+
+def wrapper(args):
+    global THREAD
+
+    if THREAD is not None and THREAD.is_alive():
+        xdialog.info(message="Task already running")
+        return
+    THREAD = Thread(target=blocking_run, args=(args,))
+    THREAD.start()
+
+
 # Set up Cli2Gui
 decorator_function = Cli2Gui(
     gui="freesimplegui",
-    run_function=blocking_run,
+    run_function=wrapper,
     auto_enable=True,
     program_name="fuckingfast-fetch",
 )
--- src/fuckingfast_batch_download/source/fitgirl/gui.py
+++ src/fuckingfast_batch_download/source/fitgirl/gui.py
@@ -1,6 +1,21 @@
+from threading import Thread
+
+import xdialog
 import cli2gui
 
 from fuckingfast_batch_download.source.fitgirl.__main__ import main, _main
+
+THREAD: Thread = None
+
+
+def wrapper(args):
+    global THREAD
+
+    if THREAD is not None and THREAD.is_alive():
+        xdialog.info(message="Task already running")
+        return
+    THREAD = Thread(target=main, args=(args,))
+    THREAD.start()
 
 
 deco = cli2gui.Cli2Gui(
