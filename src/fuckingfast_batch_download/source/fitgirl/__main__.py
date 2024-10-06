387c070d4daeb1615c49d11cff073d0801cae70e
poly000 <1348292515a@gmail.com>
2024-10-06 21:02:06 +0800

    enhance: speed-up scrap from fitgirl

--- src/fuckingfast_batch_download/source/fitgirl/__main__.py
+++ src/fuckingfast_batch_download/source/fitgirl/__main__.py
@@ -1,5 +1,6 @@
 import asyncio
 import argparse
+import logging
 
 import xdialog
 from playwright.async_api import (
@@ -9,51 +10,63 @@ from playwright.async_api import (
 )
 
 
-async def scrap(ctx: BrowserContext, url: str):
+
+
+async def scrap(ctx: BrowserContext, url: str, timeout: float):
     page = await ctx.new_page()
-    await page.goto(url)
+    logging.info(f"Navigating to {url}")
+    await page.goto(url, wait_until="domcontentloaded", timeout=timeout)
 
     links_loc = page.get_by_text("Filehoster: FuckingFast")
     links = await links_loc.all()
     if not links:
-        xdialog.error(message="ERROR: fuckingfast source not found!")
+        message = "ERROR: fuckingfast source not found!"
+        logging.error(message)
+        xdialog.error(message=message)
         return
     if len(links) > 1:
-        xdialog.error(message="ERROR: please paste url of single game!")
+        message = "ERROR: please paste url of single game!"
+        logging.error(message)
+        xdialog.error(message=message)
         return
 
-    jump = links.pop()
-    await page.goto(await jump.get_attribute("href"))
-
-    first_url_loc = page.locator("div#plaintext ul li:nth-child(1) a")
-    await first_url_loc.wait_for(timeout=0, state="attached")
-    url_loc = page.locator("div#plaintext ul li a")
-    urls = await url_loc.all_inner_texts()
-
+    hoster_links_elem = await page.locator("ul li div a").all()
+    hoster_links = await asyncio.gather(
+        *(tag.get_attribute("href") for tag in hoster_links_elem)
+    )
+    fuckingfast_links = [
+        link for link in hoster_links if link.startswith("https://fuckingfast.co")
+    ]
     output = xdialog.save_file(
         title="URL list file", filetypes=[("Text files", "*.txt")]
     )
 
+    if not output:
+        message = "User cancellation"
+        logging.info(message)
+        xdialog.info(message=message)
+        return
     with open(output, "w", encoding="utf-8") as f:
-        f.write("\n".join(urls) + "\n")
+        f.write("\n".join(fuckingfast_links) + "\n")
 
 
 async def run(args):
+    headless = not args.no_headless
     async with async_playwright() as playwright:
         if not args.skip_edge:
             try:
                 browser = await playwright.chromium.launch(
-                    headless=True, channel="msedge"
+                    headless=headless, channel="msedge"
                 )
             except PlaywrightError:
                 xdialog.warning(message="Edge was not found, fallback to Chromium")
-                browser = await playwright.chromium.launch(headless=True)
+                browser = await playwright.chromium.launch(headless=headless)
         else:
-            browser = await playwright.chromium.launch(headless=True)
+            browser = await playwright.chromium.launch(headless=headless)
         context = await browser.new_context()
         await context.tracing.start(name="fitgirl scrap", title="fitgirl scrap")
         try:
-            await scrap(ctx=context, url=args.url)
+            await scrap(ctx=context, url=args.url, timeout=args.timeout)
         except PlaywrightError as e:
             xdialog.error(title="Scrap error", message=f"{e}")
         await context.tracing.stop(path="trace-fg.zip")
@@ -70,7 +83,16 @@ def _main():
         description="Scrap fitgirl fuckingfast links and save to a file."
     )
     parser.add_argument("url", type=str, help="The URL to scrap")
+    parser.add_argument(
+        "--timeout",
+        type=float,
+        default=0,
+        help="timeout (millisecond) for waiting page to load",
+    )
     parser.add_argument("--skip-edge", help="never launch msedge", action="store_true")
+    parser.add_argument(
+        "--no-headless", help="disable headless mode", action="store_true"
+    )
     args = parser.parse_args()
 
     main(args)
