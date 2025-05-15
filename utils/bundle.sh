6faf2d79d27fe03802af05597b69fb943582c018
mokurin000 <1348292515a@gmail.com>
2025-05-15 23:27:18 +0800

    chore: there is no more playwright

--- utils/bundle.sh
+++ utils/bundle.sh
@@ -2,12 +2,12 @@ uv sync
 
 project="fuckingfast_batch_download"
 
-uv run pyinstaller --optimize 2 -D -n scrap-gui-chromium \
+uv run pyinstaller --optimize 2 -D -n scrap-gui \
     --noconsole \
     --clean --noconfirm \
     --hidden-import=tkinter --collect-data=cli2gui \
     src/$project/ui/scrap_gui.py
-uv run pyinstaller --optimize 2 -D -n extract-gui-chromium \
+uv run pyinstaller --optimize 2 -D -n extract-gui \
     --clean --noconfirm \
     --hidden-import=tkinter --collect-data=cli2gui \
     src/$project/ui/main_gui.py
